cc9288cad84bf8d25e53c959375dbc12fe488114
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-14 05:47:49 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,19 @@
 class SongsController < ApplicationController
-  before_action
+before_action :set_song!, only: [:show, :edit, :update]
+
   def index
     @songs = Song.all
   end
   
+  def show 
+    
+  end
+  
+  
+  private
+  
+  def set_song! 
+    @song = Song.find(params[:id])
+  end
   
 end
