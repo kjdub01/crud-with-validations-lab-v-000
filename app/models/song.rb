820cfb60839613d6537fb99f48bff577d47fa4d9
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-14 06:32:20 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,9 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :artit_name, presence: true 
-  
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
+  }
+  validates :released, inclusion: { in: [true, false] }
 end
