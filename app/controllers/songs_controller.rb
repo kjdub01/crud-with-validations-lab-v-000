7ec06f2fc1b9e384105c58216dc7171adcf40a8e
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-14 05:52:26 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,10 +15,18 @@ before_action :set_song!, only: [:show, :edit, :update]
     @song = Song.new
   end
   
+  def create 
+    @song = Song.new(song_params)
+  end
+  
   private
   
   def set_song! 
     @song = Song.find(params[:id])
+  end
+  
+  def song_params 
+    
   end
   
 end
